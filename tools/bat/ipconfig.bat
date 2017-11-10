c2992c4fd95e23525804e67990809e890e42cbf0
lanqiuzf <zthevery@qq.com>
2017-11-10 20:25:19 +0800

    cmd to change computer ipconfig

---
+++ tools/bat/ipconfig.bat
@@ -0,0 +1,4 @@
+netsh interface ip set address name="ÒÔÌ«Íø" source=dhcp
+netsh interface ip set dns name="ÒÔÌ«Íø" source=dhcp
+::netsh interface ip set address name="ÒÔÌ«Íø" source=static addr=192.168.1.133 mask=255.255.255.0 gateway=192.168.1.2 
+::netsh interface ip set dns name="ÒÔÌ«Íø" source=static addr=221.228.255.1
